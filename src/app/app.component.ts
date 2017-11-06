7933164c0bcfa27c23d867a33b06611fd868457c
Felipe <felipeo510.fs@gmail.com>
2017-11-06 11:31:32 -0800

    dojo mail assignment complete

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,6 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'app';
+  title = 'Dojo Mail';
+  info: Array<any> = [{email:"felipe@email.com", important:false, subject:"what?", content:"What th efff is going on here!"}, {email:"zain@email.com", important:true, subject:"huh?", content:"Dude what are you doing?"}, {email:"luke@email.com", important:false, subject:"umm?", content:"oh whaaat! nah bro!"},{email:"shane@email.com", important:true, subject:"yeah?", content:"I Think that could work!"}];
 }
